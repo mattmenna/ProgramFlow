ab44f2b79c2114e715c5c340652d54c482c6877b
Matthew Menna <mattmenna@gmail.com>
2017-05-09 13:51:37 -0400

    Run Configuration with 1 for argument. Added For Loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        int i = Integer.parseInt(args[0]);
+
+        if (i == 1) {
+            System.out.println("Hello World");
+
+        }
+
+
     }
 }
